52db79e7d591c1522a829ebf0741816306b8095e
kravtsovavibe-lgtm <kravtsovavibe@gmail.com>
2026-05-30 13:23:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 USER_PROFILE = {
     "name": "Кравцова",
